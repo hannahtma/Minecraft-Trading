079beadd11589674baef5ad4ea43adb3550f34c3
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-23 01:45:41 +0800

    did hash, rehash and statistics

--- hash_table.py
+++ hash_table.py
@@ -13,10 +13,11 @@ from referential_array import ArrayR
 from typing import TypeVar, Generic
 T = TypeVar('T')
 
+import primes
 
 class LinearProbeTable(Generic[T]):
     MIN_CAPACITY = 1
-    DEFAULT_HASH_BASE = 31
+    DEFAULT_HASH_BASE = 13
     PRIMES = [3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
               1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591, 17519, 21023,
               25229, 30313, 36353, 43627, 52361, 62851, 75521, 90523, 108631, 130363, 156437, 187751, 225307, 270371,
@@ -36,27 +37,36 @@ class LinearProbeTable(Generic[T]):
             Initialiser.
             
         """
+        self.conflict_count = 0
+        self.probe_total = 0
+        self.probe_max = 0
 
         self.count = 0
-        if tablesize_override == -1:
-            tablesize_override = expected_size
+        self.tablesize = tablesize_override
+        if self.tablesize == -1:
+            self.tablesize = expected_size
 
-        self.table = ArrayR(max(self.MIN_CAPACITY, tablesize_override))
+        self.table = ArrayR(self.tablesize)
         
     def hash(self, key: str) -> int:
         """
             Hash a key for insertion into the hashtable.
         """
+        alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-        value = 0
-        a = 31415
-        for char in key:
-            value = (ord(char) + a * value) % len(self.table)
-            a = a * LinearProbeTable.DEFAULT_HASH_BASE % (len(self.table) - 1)
+        first_alphabet = key[0].upper()
+        value = alphabets.index(first_alphabet)
+        value = value % len(self.table)
+
         return value
 
     def statistics(self) -> tuple:
-        raise NotImplementedError()
+        rehash_count = 0
+
+        if self.__len__() > self.tablesize:
+            rehash_count += 1
+
+        return (self.conflict_count, self.probe_total, self.probe_max, rehash_count)
 
     def __len__(self) -> int:
         """
@@ -79,16 +89,26 @@ class LinearProbeTable(Generic[T]):
         if is_insert and self.is_full():
             raise KeyError(key)
 
+        probe_length = 0
         for _ in range(len(self.table)):  # start traversing
             if self.table[position] is None:  # found empty slot
                 if is_insert:
+                    self.is_linear_probe = True
                     return position
                 else:
                     raise KeyError(key)  # so the key is not in
+            if self.is_linear_probe == True:
+                self.conflict_count += 1
             elif self.table[position][0] == key:  # found key
                 return position
             else:  # there is something but not the key, try next
                 position = (position + 1) % len(self.table)
+                probe_length += 1
+                self.probe_total += 1
+            self.is_linear_probe = False
+
+            if probe_length > self.probe_max:
+                self.probe_max = probe_length
 
         raise KeyError(key)
 
@@ -173,12 +193,10 @@ class LinearProbeTable(Generic[T]):
             Need to resize table and reinsert all values
         """
 
-        new_hash = LinearProbeTable(LinearProbeTable.PRIMES[self.next_prime])
-        self.next_prime += 1
+        new_hash = LinearProbeTable(primes[self.next])
 
-        for i in range(len(self.table)):
-            if self.table[i] is not None:
-                new_hash[str(self.table[i][0])] = self.table[i][1]
+        for item in range(len(self.table)):
+            new_hash.hash(item)
 
         self.count = new_hash.count
         self.table = new_hash.table
@@ -195,3 +213,7 @@ class LinearProbeTable(Generic[T]):
                 (key, value) = item
                 result += "(" + str(key) + "," + str(value) + ")\n"
         return result
+
+if __name__ == "__main__":
+    table = LinearProbeTable(10, tablesize_override=19)
+    print(table.__str__())
