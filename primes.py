15582d33f067bccb16ede7d574de87e776f628f9
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-23 01:45:52 +0800

    copied code for primes from elys branch

--- primes.py
+++ primes.py
@@ -6,4 +6,26 @@ __author__ = ''
 __docformat__ = 'reStructuredText'
 
 class LargestPrimeIterator():
-    raise NotImplementedError()
+    def __init__(self, upper_bound, factor):
+        self.upper_bound = upper_bound
+        self.factor = factor
+
+    def __next__(self):
+        for number in range(2, self.upper_bound):
+            i = 2
+            prime_flag = True
+            while (i <= number/2 and prime_flag == True):
+                if (number % i == 0):
+                    prime_flag = False
+                i += 1
+
+            if prime_flag == True:
+                self.largest_prime = number
+
+        self.upper_bound = self.largest_prime * self.factor
+
+        return self.largest_prime
+
+    
+    def __iter__(self):
+        return self
