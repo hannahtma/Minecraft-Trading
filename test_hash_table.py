3a46b8977f69a6464b2e7e8be8468cf695670256
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-29 15:06:38 +0800

    commented out test case for hash table

--- test_hash_table.py
+++ test_hash_table.py
@@ -47,21 +47,21 @@ class TestHashTable(unittest.TestCase):
         self.assertGreaterEqual(probe_max, 3)    # Jon: 3  + Whatever rehash caused
         self.assertEqual(rehash, 1)              # 1 rehash
 
-    # def test_stats(self):
-    #     table = LinearProbeTable(48, tablesize_override = FIX_TABLESIZE)
-    #     f = open("asia_countries.txt", "r")
-    #     lst = (f.read().split("\n"))
+    def test_stats(self):
+        table = LinearProbeTable(48)
+        f = open("asia_countries.txt", "r")
+        lst = (f.read().split("\n"))
         
-    #     for name in lst:
-    #         table.__setitem__(name, name+"-value")
+        for name in lst:
+            table.__setitem__(name, name+"-value")
         
-    #     f.close()
+        f.close()
         
-    #     conflict, probe_total, probe_max, rehash = table.statistics()
-    #     self.assertGreaterEqual(conflict, 4)
-    #     self.assertGreaterEqual(probe_total, 3)
-    #     self.assertGreaterEqual(probe_max, 2)
-    #     self.assertGreaterEqual(rehash, 1)
+        conflict, probe_total, probe_max, rehash = table.statistics()
+        self.assertGreaterEqual(conflict, 39)
+        self.assertGreaterEqual(probe_total, 548)
+        self.assertGreaterEqual(probe_max, 44)
+        self.assertGreaterEqual(rehash, 2)
 
 if __name__ == '__main__':
 
