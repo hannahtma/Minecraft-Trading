878f3d6e0a92ef58c417cb8e1800eebba37e26a0
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-31 21:06:52 +0800

    test for hash table

--- test_stats.py
+++ test_stats.py
@@ -7,11 +7,3 @@ for name in lst:
     table.__setitem__(name, name+"-value")
 f.close()
 print(table.statistics())
-
-table = LinearProbeTable(21)
-f = open("asia_countries.txt", "r")
-lst = (f.read().split("\n"))
-for name in lst:
-    table.__setitem__(name, name+"-value")
-f.close()
-print(table.statistics())
