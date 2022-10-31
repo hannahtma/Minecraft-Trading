d701937d2115d95e10fda3f6db7e93ad2b25153c
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-31 18:26:16 +0800

    updated the stats

--- test_stats.py
+++ test_stats.py
@@ -1,16 +1,17 @@
 from hash_table import LinearProbeTable
 
-table = LinearProbeTable(48, tablesize_override = 13)
-f = open("asia_countries.txt", "r")
+table = LinearProbeTable(21)
+f = open("names.txt", "r")
 lst = (f.read().split("\n"))
-
 for name in lst:
     table.__setitem__(name, name+"-value")
-
 f.close()
+print(table.statistics())
 
-conflict, probe_total, probe_max, rehash = table.statistics()
-self.assertGreaterEqual(conflict, 4)
-self.assertGreaterEqual(probe_total, 3)
-self.assertGreaterEqual(probe_max, 2)
-self.assertGreaterEqual(rehash, 1)
+table = LinearProbeTable(21)
+f = open("asia_countries.txt", "r")
+lst = (f.read().split("\n"))
+for name in lst:
+    table.__setitem__(name, name+"-value")
+f.close()
+print(table.statistics())
